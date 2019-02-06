ca9787dec28b689b6f80f0d1c2cbff23aac5175e
OstKost <ostkost@list.ru>
2019-02-06 12:52:51 +0300

    add modal

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Modal from '../modal'
 
 const StreamDelete = () => {
-	return <div>StreamDelete</div>
+	return (
+		<div>
+			StreamDelete
+			<Modal>MODAL window</Modal>
+		</div>
+	)
 }
 
 export default StreamDelete
